385fdd964d1149487cb2d2789039d6a09c9ca120
R. Tyler Croy <tyler@monkeypox.org>
2018-10-24 13:35:17 -0700

    Support inline transformation of legacy correlator IDs into type-specific signatures
    Fixes #15

--- src/services/events.ts
+++ src/services/events.ts
@@ -3,7 +3,8 @@
  * Jenkins instances and storing it in the database
  */
 
-import { Application, HooksObject } from '@feathersjs/feathers';
+import { createHash } from 'crypto';
+import { Application, HookContext, HooksObject } from '@feathersjs/feathers';
 import service from 'feathers-sequelize';
 import { Operators, DataTypes } from 'sequelize';
 
@@ -13,75 +14,117 @@ import logger from '../logger';
 import db from '../models';
 import Event from '../models/event';
 
-export const eventsHooks : HooksObject = {
-  before: {
-    all: [
-    ],
-    find: [
-      authorize(),
-      applyGrant(),
-      /*
-       * Pagination for feathers-sequelize relies on Model.findAndCountAll()
-       * which implements superslow SELECT COUNT(DISTINCT(id)) FROM events
-       * queries against the super-big database tables in production.
-       *
-       * Rather than rely on pagination, we'll simply add our limit and let the
-       * front-end go forwards and backwards, but never discover the total
-       * number of pages
-       */
-      (context) => {
-        Object.assign(context.params.query, {
-          $limit: 25,
-        });
+export class EventsHooks {
+  /**
+   * This feathers hook function is responsible for denormalizing the `type`
+   * passed along in the event payload.
+   *
+   * If the types service already has stored the `type` a unique constraint
+   * error will be thrown and swallowed.
+   *
+   * @param {HookContext}
+   * @return {Promise | HookContext}
+   */
+  static denormalizeType(context : HookContext) : Promise<void | HookContext<any>> {
+    return context.app.service('types')
+      .create({
+        type: context.data.type,
+      })
+    .then(() => { return context; })
+    .catch((err) => {
+      // hitting the UNIQUE constraint is an acceptable error
+      if (!err.errors.filter(e => e.type == 'unique violation')) {
+        throw err;
+      }
+    });
+  }
+
+
+  /**
+   * This feathers hook function is responsible for creating a type-specific
+   * correlator ID for legacy instances which do not properly segregate their
+   * correlator IDs between trials
+   *
+   * See also: https://github.com/jenkins-infra/uplink/issues/15
+   *
+   * @param {HookContext}
+   * @return {Promise | HookContext}
+   */
+  static transformCorrelator(context : HookContext) : Promise<void | HookContext<any>> {
+    if ((context.data.correlator) &&
+        (context.data.correlator.match(/(\w+)-(\w+)-(\w+)-(\w+)-(\w+)/))) {
+      const hasher = createHash('SHA256');
+      hasher.update(`${context.data.correlator}${context.data.type}`);
+      context.data.correlator = hasher.digest('hex');
+    }
+    return Promise.resolve(context);
+  }
+
+  /**
+   * getHooks() simply returns a HooksObject for passing into Feathers
+   *
+   * It exists to make this all a bit more simple to test, nothing more
+   *
+   * @return {HooksObject}
+   */
+  static getHooks() : HooksObject {
+    return {
+      before: {
+        all: [
+        ],
+        find: [
+          authorize(),
+          applyGrant(),
+          /*
+          * Pagination for feathers-sequelize relies on Model.findAndCountAll()
+          * which implements superslow SELECT COUNT(DISTINCT(id)) FROM events
+          * queries against the super-big database tables in production.
+          *
+          * Rather than rely on pagination, we'll simply add our limit and let the
+          * front-end go forwards and backwards, but never discover the total
+          * number of pages
+          */
+          (context) => {
+            Object.assign(context.params.query, {
+              $limit: 25,
+            });
+          },
+        ],
+        get: [
+          authorize(),
+          applyGrant(),
+        ],
+        create: [
+          /*
+          * The create route is unauthenticated so clients can POST events
+          */
+          this.transformCorrelator,
+        ],
+        update: [
+          authorize(),
+        ],
+        patch: [
+          authorize(),
+        ],
+        remove: [
+          // Used internally for tests
+          authorize({ allowInternal: true, }),
+        ],
       },
-    ],
-    get: [
-      authorize(),
-      applyGrant(),
-    ],
-    create: [
-      /*
-       * The create route is unauthenticated so clients can POST events
-       */
-    ],
-    update: [
-      authorize(),
-    ],
-    patch: [
-      authorize(),
-    ],
-    remove: [
-      // Used internally for tests
-      authorize({ allowInternal: true, }),
-    ],
-  },
-  after: {
-    create: [
-      (context) => {
-        return context.app.service('types')
-          .create({
-            type: context.data.type,
-          })
-        .then(() => { return context; })
-        .catch((err) => {
-          // hitting the UNIQUE constraint is an acceptable error
-          if ((err.errors) && (!err.errors.filter(e => e.type == 'unique violation'))) {
-            throw err;
-          }
-          else {
-            throw err;
-          }
-        });
+      after: {
+        create: [
+          this.denormalizeType,
+        ],
       },
-    ],
-  },
-  error: {},
-};
+      error: {},
+    };
+  }
+}
 
 export default (app : Application) => {
   const Model : any = Event(db.sequelize, db.sequelize.Sequelize);
   app.use('/events', service({
     Model: Model,
   }));
-  app.service('events').hooks(eventsHooks);
+  app.service('events').hooks(EventsHooks.getHooks());
 };
--- test/events.test.ts
+++ test/events.test.ts
@@ -1,8 +1,9 @@
 import url from 'url';
 import request from 'request-promise';
+import { HooksObject } from '@feathersjs/feathers';
 
 import app from '../src/app';
-import events from '../src/service/events';
+import { EventsHooks } from '../src/services/events'
 
 // Offsetting a bit to ensure that we can watch and run at the same time
 const port = (app.get('port') || 3030) + 10;
@@ -13,57 +14,85 @@ const getUrl = pathname => url.format({
   pathname
 });
 
-describe('Acceptance tests for /events', () => {
-  beforeEach((done) => {
-    this.server = app.listen(port);
-    this.server.once('listening', () => done());
-  });
-  afterEach(done => this.server.close(done));
-
-  it('responds to GET /events', () => {
-    return request(getUrl('/events'), {
-      json: true,
-      resolveWithFullResponse: true,
-    }).then(response =>
-      expect(response.statusCode).toEqual(401)
-    ).catch(err =>
-      expect(err.statusCode).toEqual(401)
-    );
-  });
-
-  describe('POST to /events', () => {
-    beforeEach(() => {
-      const removalArgs = { query: { $limit: 1000 } };
-      return app.service('events')
-        .remove(null, removalArgs)
-          .then(() => {
-            return app.service('types').remove(null, removalArgs);
-          });
+describe('Events service tests', () => {
+  describe('Acceptance tests for /events', () => {
+    beforeEach((done) => {
+      this.server = app.listen(port);
+      this.server.once('listening', () => done());
     });
+    afterEach(done => this.server.close(done));
 
-    it('should create a valid event', () => {
-      return request.post(getUrl('/events'), {
+    it('responds to GET /events', () => {
+      return request(getUrl('/events'), {
         json: true,
         resolveWithFullResponse: true,
-        body: {
-          type: 'jest-example',
-          payload: {
-            generatedAt: Date.now(),
-          },
-          correlator: '0xdeadbeef',
-        },
-      }).then((response) => {
-        expect(response.statusCode).toEqual(201)
+      }).then(response =>
+        expect(response.statusCode).toEqual(401)
+      ).catch(err =>
+        expect(err.statusCode).toEqual(401)
+      );
+    });
 
-        // ensure that a type has been created in the types table
-        return request.get(getUrl('/types'), {
+    describe('POST to /events', () => {
+      it('should create a valid event', () => {
+        return request.post(getUrl('/events'), {
           json: true,
           resolveWithFullResponse: true,
-          qs: { testing_access_token: true },
+          body: {
+            type: 'jest-example',
+            payload: {
+              generatedAt: Date.now(),
+            },
+            correlator: '0xdeadbeef',
+          },
         }).then((response) => {
-          expect(response.statusCode).toEqual(200);
-          expect(response.body.length).toEqual(1);
+          expect(response.statusCode).toEqual(201)
+
+          // ensure that a type has been created in the types table
+          return request.get(getUrl('/types'), {
+            json: true,
+            resolveWithFullResponse: true,
+            qs: { testing_access_token: true },
+          }).then((response) => {
+            expect(response.statusCode).toEqual(200);
+            expect(response.body.length).toEqual(1);
+          });
         });
+      });
+    });
+  });
+
+  describe('EventsHooks unit tests', () => {
+    it('should implement a getHooks()', () => {
+      expect(EventsHooks.getHooks()).toHaveProperty('before');
+      expect(EventsHooks.getHooks()).toHaveProperty('after');
+      expect(EventsHooks.getHooks()).toHaveProperty('error');
+    })
+
+    describe('denormalizeType()', () => {
+    });
+
+    describe('transformCorrelator', () => {
+      const context = {
+        data: {
+          correlator: null,
+          type: 'jest',
+          payload: {},
+        },
+      };
+
+      it('should transform correlators that look like UUIDs', () => {
+        const correlator = '86e3f00d-b12a-4391-bbf2-6f01c1606e17';
+        context.data.correlator = correlator;
+
+        return expect(EventsHooks.transformCorrelator(context)).resolves.not.toHaveProperty('data.correlator', correlator);
+      });
+
+      it('should NOT transform correlators that look like signatures', () => {
+        const correlator = 'e9df4c4e4e64b8168a71c3c67668dcffabbe8fca11d4ac4a7d5482562cd1679c';
+        context.data.correlator = correlator;
+
+        return expect(EventsHooks.transformCorrelator(context)).resolves.toHaveProperty('data.correlator', correlator);
       });
     });
   });
