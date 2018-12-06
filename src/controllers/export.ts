c6c65fdc6ab38f5349fa422b6d166145aa42aea0
R. Tyler Croy <tyler@monkeypox.org>
2018-12-05 17:20:12 -0800

    Switch the export controller to stream results directly from PostgreSQL to the response buffer
    I'm still not entirely sure if the PostgreSQL connection is being suitably
    returned to sequelize's connection pool, but I think this is an infrequent
    enough operation that it's worth deploying and monitoring of Sentry
    
    Fixes #29, #33

--- src/controllers/export.ts
+++ src/controllers/export.ts
@@ -5,27 +5,32 @@
 
 import authentication from '@feathersjs/authentication';
 import cookieParser from 'cookie-parser';
+import db from '../models';
+
+import QueryStream from 'pg-query-stream';
+import JSONStream from 'jsonstream'
 
 export default (app) => {
   app.post('/export',
     cookieParser(),
     authentication.express.authenticate('jwt'),
     (req, res, next) => {
-      app.service('/events/bulk')
-        .find({
-          query: {
-            type: req.body.type,
-            startDate: req.body.startDate,
-            endDate: req.body.endDate,
-          },
-          user: (req as any).user,
-        })
-        .then((result) => {
-          res.setHeader('Content-Disposition', `attachment; filename=${req.body.type}-${req.body.startDate}.json`);
-          res.setHeader('Content-Type', 'application/json');
-          res.send(result);
+      db.sequelize.connectionManager.getConnection().then((pgConn) => {
+        const query = new QueryStream('SELECT * FROM events WHERE type = $1 AND "createdAt" > $2 AND "createdAt" <= $3', [
+          req.body.type,
+          req.body.startDate,
+          req.body.endDate
+        ]);
+        const stream = pgConn.query(query);
+        res.writeHead(200, {
+          'Content-Disposition' : `attachment; filename=${req.body.type}-${req.body.startDate}.json`,
+          'Content-Type': 'application/json',
+        });
+        stream.pipe(JSONStream.stringify(false)).pipe(res);
+        stream.on('end', () => {
           res.end();
-        })
-        .catch(next);
+        });
+      })
+      .catch((err) => { console.log(err.stack); next(err); });
     });
 };
--- src/services/index.ts
+++ src/services/index.ts
@@ -1,11 +1,9 @@
-import bulk from './bulk';
 import events from './events';
 import grants from './grants';
 import types from './types';
 import users from './users';
 
 export default (app) => {
-  app.configure(bulk);
   app.configure(events);
   app.configure(grants);
   app.configure(types);
